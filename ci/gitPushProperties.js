202c1c1c1e33744fc4822f2b168f15867880ffa9
feerglas <info@feerglas.com>
2020-09-07 22:18:24 +0200

    fix: add files to commit dist files and trigger lyne components

---
+++ ci/gitPushProperties.js
@@ -0,0 +1,51 @@
+const shell = require('shelljs');
+const fs = require('fs');
+const path = require('path');
+const simpleGit = require('simple-git');
+const argv = require('minimist');
+const getCommit = require('./getTravisCommit');
+
+require('dotenv')
+  .config();
+
+const git = simpleGit();
+
+const getAllFiles = (dirPath, arrayOfFiles) => {
+  const files = fs.readdirSync(dirPath);
+
+  let _arrayOfFiles = arrayOfFiles || [];
+
+  files.forEach((file) => {
+    const filePath = `${dirPath}/${file}`;
+    const isDirectory = fs
+      .statSync(filePath)
+      .isDirectory();
+
+    if (isDirectory) {
+      _arrayOfFiles = getAllFiles(filePath, _arrayOfFiles);
+    } else {
+      _arrayOfFiles.push(path.join(dirPath, '/', file));
+    }
+  })
+
+  return _arrayOfFiles;
+};
+
+(async () => {
+  try {
+    const jobId = argv(process.argv.slice(2))['i'];
+    const commitMessage = await getCommit(jobId);
+    const propertiesFiles = getAllFiles('./dist');
+
+    // git add and commit. Files will be pushed during semantic-release
+    await git.add(propertiesFiles);
+    await git.commit(`${commitMessage} [skip ci]`);
+
+    shell.exit(0);
+  } catch (e) {
+    console.log('-->> Error while committing icon files.');
+    console.log(e);
+
+    shell.exit(1);
+  }
+})();
