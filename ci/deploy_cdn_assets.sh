adf3465b74dcaf22b90764f08181a8df72680f66
feerglas <info@feerglas.com>
2020-09-23 16:32:17 +0200

    feat: add script to deploy to netlify

---
+++ ci/deploy_cdn_assets.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# this script is used in .travis.yml
+
+# this script is being made executable with
+# chmod ugo+x deploy_cdn_assets.sh
+
+# set flags for execution of this file:
+# -e: make sure command returns non-zero exit code as soon as somehting fails
+set -e
+
+# version file name written by .releaserc
+VERSION_FILE=.version
+
+if [ -f "$VERSION_FILE" ];
+then
+
+  VERSION="$(cat .version)"
+
+  # Deploy Storybook on Netlify
+  netlify deploy --prod --message "::$VERSION::" --site $NETLIFY_SITE_ID --auth $NETLIFY_AUTH_TOKEN --dir ./storybook-static/
+
+else
+  echo "-->> No version released, thus skipping netlify depoly"
+fi
