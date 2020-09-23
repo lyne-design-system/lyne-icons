0a9b0c89002d4e6d85d7f04a99df60cc5e70f450
feerglas <info@feerglas.com>
2020-09-23 17:34:55 +0200

    fix: checkout first

--- ci/createCdnAssets.js
+++ ci/createCdnAssets.js
@@ -79,6 +79,7 @@ const copyFiles = (source, destination) => {
   // commit and push files in cdn folder
   const cdnFiles = getAllFiles(cdnDir);
 
+  await git.checkout('master');
   await git.pull();
   await git.add(cdnFiles);
   await git.commit(`chore: Add assets for version ${version} [skip ci]`);
