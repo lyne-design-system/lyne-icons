887c58a7d03546bb429713eabc52c77c694b834e
feerglas <info@feerglas.com>
2020-09-23 21:31:19 +0200

    feat: manually increment version, try to fix semantic-release mismatch
    BREAKING CHANGE: trigger major release

--- ci/createCdnAssets.js
+++ ci/createCdnAssets.js
@@ -86,7 +86,6 @@ const copyFiles = (source, destination) => {
 
     await git.checkout('master');
     await git.pull();
-
     await git.add(cdnFiles);
     await git.commit(`chore: add CDN assets for version ${version} [skip ci]`);
     await git.push('origin', 'master', {
